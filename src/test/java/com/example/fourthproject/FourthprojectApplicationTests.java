a358976428652623978ce5090a584d141f596515
saye <saye.ghadimi79@gmali.com>
2024-06-10 23:06:19 +0300

    first commit

---
+++ src/test/java/com/example/fourthproject/FourthprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fourthproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourthprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
